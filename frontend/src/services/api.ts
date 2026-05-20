c54e16faa6ed455cbff7d8474483b8005c7dc2d7
Daniel Pinheiro <lopesdanielhps@gmail.com>
2026-05-20 09:39:40 -0300

    DEU CERTO

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://desempenhocomercial.duckdns.org",
 })
 
 export function getToken() {
